95e5d89d24c8c91a78a2e2ccf5bd5b7a68ec32d8
qasimfida <qasimfida0054@gmail.com>
2021-02-04 00:56:31 +0500

    Added UI changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,3 +85,33 @@ $("#moveToBottom").click(function () {
   window.scrollTo({ top: documentHeight, behavior: "smooth" });
   return false;
 });
+
+$(".slick-slider").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        dots: false,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
